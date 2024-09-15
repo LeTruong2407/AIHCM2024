b9adf199cceb5c91ff6a80aca9aee937c69eaf4b
LeTruong2407 <levantruong24072003@gmail.com>
2024-09-15 14:17:32 +0700

    first commit

---
+++ frontend/ai/src/helper/web_url.js
@@ -0,0 +1,12 @@
+//BACKEND
+export const web_url = "http://127.0.0.1:8080/";
+
+//SOCKET | DB SERVER
+export const socket_url =
+  "http://127.0.0.1:5000/";
+
+//COMPETITION SUBMISSION SEVER
+export const server = "";
+
+//SESSION ID FOR COMPETITION SUBMISSION SEVER
+export const session = "";
